5e8a6ac08c11be22ac558b799070e29c52dfbc0c
Adith <adityachavan6890@gmail.com>
2025-05-14 02:09:50 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'Movie-App',
+  base:'/Movie-App',
   plugins: [react(), tailwindcss()],
 })
